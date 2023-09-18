5e91516b47454363f4c43ff32ec97e44a977a0c9
shofegen <danik_lik1@mail.ru>
2023-09-18 21:19:12 +0500

    Дз на проверку

--- src/Main.java
+++ src/Main.java
@@ -83,5 +83,6 @@ public class Main {
         System.out.println("Маша теперь получает " + MashaMonthAfter + " рублей. Годовой доход вырос на " + MashaBefAft + " рублей");
         System.out.println("Денис теперь получает " + DenisMonthAfter + " рублей. Годовой доход вырос на " + DenisBerAft + " рублей");
         System.out.println("Кристина теперь получает " + KristinaMonthAfter + " рублей. Годовой доход вырос на " + KristinaBerAft + " рублей");
+
     }
 }
